73dd2de9312a764ad8c17b2b11dd00c907cab769
susnitdgp@gmail.com <susnitdgp@gmail.com>
2021-02-06 17:05:38 +0000

    Second Commit

---
+++ src/main/java/com/example/SpringBootDemo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.SpringBootDemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "hello";
+	}
+}
