364e405d89a3e5bc78f947f534faa95e85e0de90
Susanta Goswami <susnitdgp@gmail.com>
2021-02-08 16:34:04 +0000

    Fourth Commit

--- src/main/java/com/example/SpringBootDemo/HomeController.java
+++ src/main/java/com/example/SpringBootDemo/HomeController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 
 	@GetMapping("/greeting")
 	public String greeting() {
